fe4226f6549bd291a0ab451fe8e6f6d852c51592
Harsh Prajapati <harshp7043@gmail.com>
2026-07-23 16:12:34 +0530

    second commit

--- dvc.py
+++ dvc.py
@@ -8,9 +8,10 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
     }
 
 df = pd.DataFrame(data)
-
+new_row_loc = {'Name': 'V2', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 data_dir='data'
-os.makedirs(data_dir,exist_ok=False)
+os.makedirs(data_dir,exist_ok=True)
  
 file_path=os.path.join(data_dir,'simple_data.csv')
 
