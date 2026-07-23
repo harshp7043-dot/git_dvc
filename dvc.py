6313324e891a1e8b067ff3db1ec58a97ada36cf7
Harsh Prajapati <harshp7043@gmail.com>
2026-07-23 10:39:50 +0530

    1-before dvc

---
+++ dvc.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+# Create a sample DataFrame with column names
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+    }
+
+df = pd.DataFrame(data)
+
+data_dir='data'
+os.makedirs(data_dir,exist_ok=False)
+ 
+file_path=os.path.join(data_dir,'simple_data.csv')
+
+df.to_csv(file_path,index=False)
+
+print(f"csv file saved to {file_path}")
